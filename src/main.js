7965404661a07882a25141419697c28706510448
Christian Wallervand <cwallervand@gmail.com>
2025-09-16 10:03:48 +0200

    Added popover js api example:

--- src/main.js
+++ src/main.js
@@ -4,20 +4,28 @@ import './components/topicSection/topicSection.js';
 function isPopoverSuported() {
 	return Object.hasOwn(HTMLElement.prototype, 'popover');
 }
+
+export function displayPopover(popoverId) {
+	console.log('show popover', popoverId);
+	const popover = document.getElementById(popoverId);
+	if (isPopoverSuported() && popover) {
+		popover.showPopover();
+	}
+}
+
 const nonScamPopover = document.getElementById('not-a-scam-popover');
 
 const scamWraper = document.getElementById('scam');
 const scamButton = document.getElementById('scam-button');
-const scamPopover = document.getElementById('scam-popover');
 
 if (isPopoverSuported()) {
-	scamPopover.popover = 'auto';
-	scamButton.popoverTargetElement = scamPopover;
-	scamButton.popoverTargetAction = 'toggle';
-
 	nonScamPopover.addEventListener('beforetoggle', (event) => {
 		console.log('popover event', event);
 	});
 } else {
 	scamWraper.style.display = 'none';
 }
+
+scamButton.addEventListener('click', () => {
+	displayPopover('scam-popover');
+});
